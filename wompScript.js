37aca9ebc1112f660abfa33eaf086a45e14b973f
Frederic Desautels <fredesautels@gmail.com>
2020-09-19 08:04:00 -0400

    Update wompScript.js

--- wompScript.js
+++ wompScript.js
@@ -13,3 +13,10 @@ if (location.href == "https://www.cemetech.net/forum/search.php?search_id=weekpo
         content.parentNode.insertBefore(document.createElement("br"), content.nextElementSibling);
     }
 }
+//Make online names clickable
+var sidebar = document.querySelectorAll("p.sidebar__section-body")[0];
+var pre = sidebar.textContent.split("Members:");
+var names = pre.pop().trim().slice(0, -1).split(", ");
+for (var i = 0; i < names.length; i++)
+    names[i] = "<a href='https://www.cemetech.net/forum/profile.php?mode=viewprofile&u=" + names[i] + "'>" + names[i] + "</a>";
+sidebar.innerHTML = pre + "<br>Members: " + names.join(", ") + ".";
